7f63fcf2d8b0bf34615527c966f66b5e79463d1f
Дмитрий <a@mail.ru>
2022-02-14 13:42:26 +0300

    Авторизация FireBase

---
+++ app/src/main/java/ru/ermilov/servicecenter/Users.java
@@ -0,0 +1,14 @@
+package ru.ermilov.servicecenter;
+
+public class Users {
+    public String name, password;
+
+    public Users() {
+
+    }
+
+    public Users(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
