6b28456c0a2d798f2f496301220e3f610ed77c6a
Дмитрий <a@mail.ru>
2022-02-19 13:27:26 +0300

    Сделал страницу для добавления клиента

--- app/src/main/java/ru/ermilov/servicecenter/FormClientFragment.java
+++ app/src/main/java/ru/ermilov/servicecenter/FormClientFragment.java
@@ -2,8 +2,6 @@ package ru.ermilov.servicecenter;
 
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,23 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class AddClientFragment extends Fragment {
-
-    private CardView addClient;
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class FormClientFragment extends Fragment {
 
 
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_client, container, false);
-
-
+        return inflater.inflate(R.layout.fragment_form_client, container, false);
     }
 }
