6d7057f4c2cedf7bc1c2c94ed5d100f0a9775fc4
Дмитрий <a@mail.ru>
2022-03-11 14:35:13 +0300

    Создание клиента

---
+++ app/src/main/java/ru/ermilov/servicecenter/Clients.java
@@ -0,0 +1,15 @@
+package ru.ermilov.servicecenter;
+
+public class Clients {
+    public String fio, phone, email;
+
+    public Clients() {
+
+    }
+
+    public Clients(String fio, String phone, String email) {
+        this.fio = fio;
+        this.phone = phone;
+        this.email = email;
+    }
+}
